fe18a3e22055f9450fc4a30aec3d0f670c067a3f
Francisco Renan Donaire Tablada <francisco_renan-dt@hotmail.com>
2019-06-08 20:48:42 -0600

    Estructura Basica

--- js/first.js
+++ js/first.js
@@ -1,1 +1,9 @@
-/*console.log("hello worl")*/
+/*                   */
+console.log("hello worl")
+/*                  */
+var button = document.getElementById("button");
+var rainbow = ["red","orange","yellow","green","blue","indigo","violet"];
+function change(){
+    document.body.style.background = rainbow[Math.floor(7*Math.random())];
+}
+button.addEventListener("click",change);

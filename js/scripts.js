9202e036628782bfa920214a03f167cabf6d3bf6
Francisco Renan Donaire Tablada <francisco_renan-dt@hotmail.com>
2019-06-19 17:16:44 -0600

    Final del Capitulo 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,50 @@
-// welcome the user
+var quiz = [
+    ["What is Superman's real name?","Clarke Kent"],
+    ["What is Wonderwoma's real name?","Dianna Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+];
+
+var score = 0 // initialize score
+
+play(quiz);
+
+function play(quiz){
+    // main game loop
+    for(var i= 0, question, answer, max=quiz.length; i<max; i++){
+        question = quiz[i][0];
+        answer = ask(question);
+        check(answer);
+    }
+    // end of main game loop
+    gameOver();
+    
+    function ask(question){
+        return prompt(question);
+    }
+
+    function check(answer){
+        if(answer === quiz[i][1]){ // quiz[i][1] is the ith answer
+            alert("Correct!");
+            //increase score by 1
+            score++;
+        }else{
+            alert("Wrong!");
+        }
+    }
+    function gameOver(){
+        // inform the player that the game 
+        // has finished and tell them how many points they have
+        alert("Game Over, you scored "+score+" points");
+    }
+}
+
 /*
-var question = 'What is Superman\'s real name?';
-var answer = prompt(question);
-alert("You answered "+ answer);
-*/
+// Final del Capitulo 3
+
 var quiz= [
     ["What is Superman's real name?","Clarke Kent"],
     ["What is Wonderwoma's real name?","Dianna Prince"],
-    ["What is Batmsn's real name?","Bruce Wayne"]
+    ["What is Batman's real name?","Bruce Wayne"]
 ];
 var score=0;
 for (var i=0; i<quiz.length; i++){
@@ -20,3 +57,11 @@ for (var i=0; i<quiz.length; i++){
     }
 }
 alert("Game over, you scored "+ score + " points");
+*/
+/*
+// Final del Capitulo 2
+
+var question = 'What is Superman\'s real name?';
+var answer = prompt(question);
+alert("You answered "+ answer);
+*/
